c2aae0ff151f9c0599a3386eb684c787893b984c
Roman Surkov <s.rb@list.ru>
2022-07-11 15:05:35 +0300

    Added library styled-components. Гарантирует уникальные стили

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,11 +1,38 @@
 import "./Button.css";
+import styled from 'styled-components';
 
-const Button = (props) => {
-  return (
-    <button type={props.type} className="button">
-      {props.children}
-    </button>
-  );
-};
+// у объекта styled вызывается метод button в который передается то что в кавычках
+// Будет создан объект кнопки со стилями переданными между кавычками
+// указание на стиль себя (кнопки) - заменяется на & если есть чтото. Если ничего нет, просто стили открыто передаются
+// такой способ tagged template literals
+const Button = styled.button`
+
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  color: white;
+  background: #00358b;
+  border-radius: 4px;
+  box-shadow: 0 0 4px rgba(50, 50, 50, 0.25);
+  cursor: pointer;
+
+
+ &:focus {
+  outline: none;
+}
+
+&:hover,
+&:active {
+  background: #245fbd;
+  box-shadow: 0 0 8px rgba(50, 50, 50, 0.25);
+}
+`;
+
+// const Button = (props) => {
+//   return (
+//     <button type={props.type} className="button">
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
