c3d9df411bbeb6b9003c083707bd9f9a8e606625
Roman Surkov <s.rb@list.ru>
2022-07-11 15:52:21 +0300

    Использование styled-components и передача класса

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -2,6 +2,43 @@ import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
 import "./TaskInput.css";
+import styled from "styled-components";
+
+const FormControl = styled.div`
+
+  margin: 0.5rem 0;
+
+& label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+& input {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+}
+
+& input:focus {
+  outline: none;
+  background: #c8e1e4;
+  border-color: #00358b;
+}
+
+/*Ссылка на элемент у которого есть оба стиля, а также в нем инпут*/
+&.invalid input {
+  border-color: red;
+  background: rgb(243, 157, 157);
+}
+
+&.invalid label {
+  color: red;
+}
+`;
 
 const TaskInput = (props) => {
   const [inputText, setinputText] = useState("");
@@ -25,10 +62,10 @@ const TaskInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isInputValid ? 'invalid' : ''}`}>
+      <FormControl className={!isInputValid && 'invalid'}>
         <label>Задачи</label>
         <input type="text" onChange={taskInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Добавить Задачу</Button>
     </form>
   );
